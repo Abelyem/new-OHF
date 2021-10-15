23ca292119d5135e25de98a1f0385d2430edb3d0
Abel Yemane <abel.yemane1@hotmail.co.uk>
2021-10-15 16:33:06 +0100

    added new function for single trustee which would display and hide information on click of a button

--- static/js/main.js
+++ static/js/main.js
@@ -5,3 +5,15 @@ $("#gallery-img-container").magnificPopup({
     enabled: true
   }
 });
+
+const yewandeInfo = document.getElementById('yewandeInfo')
+const overlay = document.getElementById('overlay')
+
+
+function ShowHideTrusteeInfo() {
+  if (yewandeInfo.style.display === "none") {
+    yewandeInfo.style.display = "inline";
+  } else {
+    yewandeInfo.style.display = "none";
+  }
+}
